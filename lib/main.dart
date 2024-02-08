3014877992162a7514d28801fef0b5602d69a6cf
venicode <vitoria_aparecida2011@hotmail.com>
2024-02-08 13:07:35 -0300

    criacao do post e do endpoint journal

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webapi_first_course/helpers/async.study.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'models/journal.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
   JournalService service = JournalService();
-  service.register("Hello World");
+  service.register(Journal.empty());
   service.get();
   asyncStudy();
 }
@@ -19,16 +22,35 @@ class MyApp extends StatelessWidget {
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.grey,
-          appBarTheme: const AppBarTheme(
-            elevation: 0,
-              backgroundColor: Colors.black,
-              titleTextStyle: TextStyle(color: Colors.white))),
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white)
+        ),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+        "add-journal": (context) => AddJournalScreen(
+              journal: Journal(
+                  id: "id",
+                  content: "content",
+                  createdAt: DateTime.now(),
+                  updatedAt: DateTime.now()),
+            ),
+      },
+    onGenerateRoute: (settings) {
+        if(settings.name == "add-journal") {
+          final Journal journal = settings.arguments as Journal;
+          return MaterialPageRoute(builder: (context) {
+            return AddJournalScreen(journal: journal);
+          });
+        }
       },
     );
   }
--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -4,25 +4,30 @@ import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
 
+import '../models/journal.dart';
+
 class JournalService {
   static const String url = 'http://192.168.0.10:3000/';
-  static const String resource = 'learnhttp/';
+  static const String resource = 'journals/';
 
-  http.Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   String getUrl() {
     return '$url$resource';
   }
 
-  register(String content) {
-    client.post(
-      Uri.parse(getUrl()),
-      body: json.encode({"content": content}),
-      headers: {"Content-Type": "application/json; charset=UTF-8"},
-    );
+  Future<bool> register(Journal journal) async {
+    String jsonJournal = json.encode(journal.toMap());
+    http.Response response = await client.post(Uri.parse(getUrl()),
+        headers: {'Content-type': 'application/json'}, body: jsonJournal);
+    if (response.statusCode == 201) {
+      return true;
+    }
+    return false;
   }
-  
-  Future <String> get() async{
+
+  Future<String> get() async {
     http.Response response = await client.get(Uri.parse(getUrl()));
     print(response.body);
     return response.body;
